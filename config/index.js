aac5ce39bdf4cd69e67585332819cf536a4580c4
皮娟 <trytry8090@163.com>
2018-04-16 22:51:50 +0800

    assetsPublicPath

--- config/index.js
+++ config/index.js
@@ -44,7 +44,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/zhongtai/foreEnd/',
+    assetsPublicPath: '/zhongtai/fe-mobile/',
 
     /**
      * Source Maps
--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,7 @@ Vue.use(Router)
 
 export default new Router({
   mode:'history',
-  base: '/zhongtai/foreEnd/',
+  base: '/zhongtai/fe-mobile/',
   routes: [
     {
       path: '/',
